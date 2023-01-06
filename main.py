fb2175b627a22cc9c79b34951934d143a75e39f6
cristian <gonzalez.resina@gmail.com>
2023-01-06 15:31:06 +0100

    Introducida nueva función similar

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import random
+
 def check_double_zero(*args):
     active = False
     for i in args:
         if i == 0:
-            if active == True:
+            if active:
                 return True
             else:
                 active = True
@@ -10,9 +12,33 @@ def check_double_zero(*args):
             active = False
     return False
             
+def check_double_zero2(aleatorios):
+    active = False
+    for i in aleatorios:
+        if i == 0:
+            if active:
+                return True
+            else:
+                active = True
+        else:
+            active = False
+    return False
+
+def check_double_zero3(*args):
+    contador = 0
+    for i in args:
+        if i == 0 and args[contador+1] == 0:
+            return True
+        else:
+            contador += 1
+    return False
+
+print(check_double_zero3(1, 4, 6, 3, 9, 1, 0, 0, 0, 0, 1, 2, 434, 56, 32))
+print(check_double_zero3(1, 4, 6, 3, 9, 1, 0, 60, 50, 70, 1, 2, 434, 56, 32))
+print(check_double_zero3(0, 0, 1, 4, 6, 3, 9, 1, 0, 60, 50, 70, 1, 2, 434, 56, 32))
+print(check_double_zero3(0, 4, 0, 1, 4, 6, 3, 9, 1, 0, 60, 50, 70, 1, 2, 434, 56, 32))
 
 
-print(check_double_zero(1, 4, 6, 3, 9, 1, 0, 0, 0, 0, 1, 2, 434, 56, 32))
-print(check_double_zero(1, 4, 6, 3, 9, 1, 0, 60, 50, 70, 1, 2, 434, 56, 32))
-print(check_double_zero(0, 0, 1, 4, 6, 3, 9, 1, 0, 60, 50, 70, 1, 2, 434, 56, 32))
-print(check_double_zero(0, 4, 0, 1, 4, 6, 3, 9, 1, 0, 60, 50, 70, 1, 2, 434, 56, 32))
+aleatorios = [random.randint(0,99) for _ in range(1000)]
+
+print(check_double_zero2(aleatorios))
